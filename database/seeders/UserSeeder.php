64562f7b810ac73d966e4941b1267235fadbb438
Kea Rajab <kearajab@gmail.com>
2025-10-18 21:36:32 +0300

    nothing

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $user = User::updateOrCreate(
-            ['email' => 'kea.rajab@icloud.com'],
+            ['email' => 'kea@bible.comm'],
             [
                 'name' => 'Admin Kea',
                 'password' => Hash::make('asdfasdf'),
